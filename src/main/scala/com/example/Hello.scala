d4fb8797700388716b522a4f7aa32c5b8373625b
shigemk2 <i.am.shige@gmail.com>
2016-04-15 14:54:02 +0900

    Add sample

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,15 @@
 package com.example
 
+import scalaj.http.{Http, HttpResponse}
+
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val response: HttpResponse[String] = Http("http://www.shigemk2.com").asString
+    println(response.body)
+    println(response.code)
+    println(response.headers)
+    println(response.cookies)
     println("Hello, world!")
   }
 }
